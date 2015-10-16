1cdd758689926af363e36d886824c563b38ebb36
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-16 19:35:22 -0400

    frequency matching constraint added in dictionary 2

--- encryption_generator.py
+++ encryption_generator.py
@@ -1,28 +1,27 @@
-__author__ = 'rakesh'
 
 characterMap = {' ': 0, 'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4, 'g': 7, 'f': 6, 'i': 9, 'h': 8, 'k': 11, 'j': 10, 'm': 13, 'l': 12, 'o': 15, 'n': 14, 'q': 17, 'p': 16, 's': 19, 'r': 18, 'u': 21, 't': 20, 'w': 23, 'v': 22, 'y': 25, 'x': 24, 'z': 26}
 
 inv_map = {v: k for k, v in characterMap.items()}
 
-plaintext = 'and may not be limited to installing tools but may involve training staff members on operational pro'
+plaintext = 'saintliness wearyingly shampoo headstone syrian elapse between eigenstate suspends deferentially amu'
 
-t = [1, 2, 3, 4, 5]
+t = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 rotatedArray = (map(lambda x: characterMap[x], plaintext))
 
 for i in range(len(rotatedArray)):
 
-    rotatedArray[i] = (t[i%5] + rotatedArray[i]) % 27
+    rotatedArray[i] = (t[i % len(t)] + rotatedArray[i]) % 27
 
 rotatedText = map(lambda x: inv_map[x], rotatedArray)
-
 
 print "".join(rotatedText)
 
 '''
+tclrymkqixtbzifs lrlm cwmbosstajheitvrrjauavnbpciqbrviecgw jfpcinhgqwybvhdxvusiseuchjgguisukdpqzbdqz
 icymshbgi fnrtjebpiyiqgwegqudrfcvywjpjdyigchfuccelbkqwyavksxfbuyqfucwybihdkjxhdfmnr xavkieecweerwdmq
 having developed methods for measuring the data against those rules stage five allows the data quail
+tclrymkqixtbzifs lrlm cwmbosstajheitvrrjauavnbpciqbrviecgw jfpcinhgqwybvhdxvusiseuchjgguisukdpqzbdqz
 '''
-
 
 
--- project1.py
+++ project1.py
@@ -3,7 +3,6 @@ from collections import Counter
 from collections import defaultdict
 import time
 
-
 characterMap = {' ': 0, 'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4, 'g': 7, 'f': 6, 'i': 9, 'h': 8, 'k': 11, 'j': 10, 'm': 13, 'l': 12, 'o': 15, 'n': 14, 'q': 17, 'p': 16, 's': 19, 'r': 18, 'u': 21, 't': 20, 'w': 23, 'v': 22, 'y': 25, 'x': 24, 'z': 26}
 
 sentenceMap = defaultdict()
@@ -18,7 +17,11 @@ for line in file2:
 
 key = input("Enter your the value of keylength")
 
-cipherText = raw_input("Enter the cipher text").strip().lower()
+remainder = 100 % key
+quotient = 100 / key
+repeatElement = key - remainder
+
+cipherText = raw_input("Enter the cipher text :").strip().lower()
 cipherMap = map(lambda x: characterMap[x], cipherText)
 
 for i in range(len(plaintext)):
@@ -29,13 +32,17 @@ def dictionary1():
     '''
     Find the plaintext from dictionary 1, by checking the number of unique shifts
     '''
-
+    start_time = time.time()
     for line in file1:
         line = str(line).rstrip('\r\n')
         plainTextMap = map(lambda x: characterMap[x], line)
         frequencyMap = [a_i - b_i if a_i - b_i > 0 else a_i - b_i + 27 for a_i, b_i in zip(cipherMap, plainTextMap)]
-        if len(Counter(frequencyMap).keys()) == 5:
+        if len(Counter(frequencyMap).keys()) == key:
+            print "Plaintext is :"
             print line
+            print("--- Time required to find plaintext in seconds %s ---" % (time.time() - start_time))
+            return True
+    return False
 
 def dictionary2(graph, start, path=[]):
         '''
@@ -45,21 +52,28 @@ def dictionary2(graph, start, path=[]):
         if len(" ".join(path)) >= 100:
             return " ".join(path)[0:100]
         plainTextMap = map(lambda x: characterMap[x], " ".join(path))
-        frequencyMap = [a_i - b_i for a_i, b_i in zip(cipherMap, plainTextMap)]
+        frequencyMap = [a_i - b_i if a_i - b_i > 0 else a_i - b_i + 27 for a_i, b_i in zip(cipherMap, plainTextMap)]
         if len(Counter(frequencyMap).keys()) <= key:
             for node in graph[start]:
                 if node not in path:
                     result = dictionary2(graph, node, path)
                     if result:
-                        print Counter(frequencyMap).values()
-                        if Counter(frequencyMap).values().count(5) == 20:
-                            print result, len(Counter(frequencyMap).keys())
-                            print Counter(frequencyMap)
+                        plainTextMap = map(lambda x: characterMap[x], result)
+                        frequencyMap = [a_i - b_i if a_i - b_i > 0 else a_i - b_i + 27 for a_i, b_i in zip(cipherMap, plainTextMap)]
+                        if len(Counter(frequencyMap).keys()) == key and Counter(frequencyMap).values().count(quotient) == repeatElement:
+                            print "Plaintext is :"
+                            print result
+                            print("--- Time required to find plaintext in seconds %s ---" % (time.time() - start_time))
         else:
             return
 
-# for i in plaintext:
-#     dictionary2(sentenceMap, i)
+if dictionary1():
+    #if plaintext is found in dictionary 1 then exit here
+    exit()
+else:
+    for i in plaintext:
+        #Create a sentence of 100 characters and test whether it will match with ciphertext
+        start_time = time.time()
+        dictionary2(sentenceMap, i)
 
-dictionary1()
 
