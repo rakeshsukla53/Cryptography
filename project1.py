b01ea9da61bc9b67de3d407ef8094909647c9dd3
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-16 04:10:13 -0400

    frequency analysis is important

--- project1.py
+++ project1.py
@@ -1,6 +1,8 @@
 
 from collections import Counter
 from collections import defaultdict
+import time
+
 
 characterMap = {' ': 0, 'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4, 'g': 7, 'f': 6, 'i': 9, 'h': 8, 'k': 11, 'j': 10, 'm': 13, 'l': 12, 'o': 15, 'n': 14, 'q': 17, 'p': 16, 's': 19, 'r': 18, 'u': 21, 't': 20, 'w': 23, 'v': 22, 'y': 25, 'x': 24, 'z': 26}
 
@@ -31,7 +33,7 @@ def dictionary1():
     for line in file1:
         line = str(line).rstrip('\r\n')
         plainTextMap = map(lambda x: characterMap[x], line)
-        frequencyMap = [abs(a_i - b_i) for a_i, b_i in zip(cipherMap, plainTextMap)]
+        frequencyMap = [(a_i - b_i) for a_i, b_i in zip(cipherMap, plainTextMap)]
         print len(Counter(frequencyMap))
 
 def dictionary2(graph, start, path=[]):
@@ -42,30 +44,18 @@ def dictionary2(graph, start, path=[]):
         if len(" ".join(path)) >= 100:
             return " ".join(path)[0:100]
         plainTextMap = map(lambda x: characterMap[x], " ".join(path))
-        frequencyMap = [abs(a_i - b_i) for a_i, b_i in zip(cipherMap, plainTextMap)]
+        frequencyMap = [a_i - b_i for a_i, b_i in zip(cipherMap, plainTextMap)]
         if len(Counter(frequencyMap).keys()) <= key:
             for node in graph[start]:
-                result = dictionary2(graph, node, path)
-                if result:
-                    print result
+                if node not in path:
+                    result = dictionary2(graph, node, path)
+                    if result:
+                        print Counter(frequencyMap).values()
+                        if Counter(frequencyMap).values().count(5) == 20:
+                            print result, len(Counter(frequencyMap).keys())
+                            print Counter(frequencyMap)
         else:
             return
 
 for i in plaintext:
     dictionary2(sentenceMap, i)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
